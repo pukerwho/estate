6aec8dac250bfaca4db43befc55e261fb59be10c
Vladimir Samojlov <v-moi@ya.ru>
2019-02-11 00:29:46 +0200

    add language

--- header.php
+++ header.php
@@ -7,7 +7,11 @@
 
   <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
   <link rel="profile" href="http://gmpg.org/xfn/11">
-
+  <base href="/">
+  <link rel="alternate" hreflang="x-default" href="<?php echo home_url(); ?>">
+  <link rel="alternate" hreflang="en" href="<?php echo home_url(); ?>/en">
+  <link rel="alternate" hreflang="ru" href="<?php echo home_url(); ?>/ru">
+  <link rel="alternate" hreflang="ua" href="<?php echo home_url(); ?>/ua">
   <?php
   // ENQUEUE your css and js in inc/enqueues.php
     wp_head();
